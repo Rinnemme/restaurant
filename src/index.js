3bdae1efd4ae48b59ee4babd242ea3081ca141ff
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-08-15 13:07:34 -0400

    load in basic assets, hard code an html page to get styles set up in css

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const content = document.getElementById("content")
+export {content}
+
+//  <div id="content">
+//         <div id="header">
+//             <img id="logo" src="../src/earth.png">
+//             <button id="home-button">Home</button>
+//             <button id="menu-button">Menu</button>
+//             <button id="locations-button">Locations</button>
+//         </div>
+//         <div id="info-content">
+//             <div id="title-card">
+//                 Nomnivore
+//             </div>
+//             <div class="info-block">
+//                 <div class="info-header black-br">Header Time</div>
+//                 <div class="info-main">Here's some text</div>
+//             </div>
+//             <div class="info-block">
+//                 <div class="info-header black-br">Header Time</div>
+//                 <div class="info-main">
+//                     <div class="info-text">
+//                         Let's just write some info at a bit more length about 
+//                         a prospective dish. This dish is probably prepared by some sort of chef
+//                         who has some manner of expertise from some specific location, a location
+//                         from whence they hail. Having that expertise, this chef crafts said dish
+//                         in order to benefit the customer's taste buds, and so on and so forth"
+//                     </div>
+//                     <div class="info-image"></div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
